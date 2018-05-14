aa9c5b5ffaabe78cc84db68802f204033c62bd0c
ailiush <ailiush@mera.ru>
2018-05-14 16:31:32 +0300

    add loopin_data and passengers_counters support

--- egts/egts_message/service/teledata/loopin_data.py
+++ egts/egts_message/service/teledata/loopin_data.py
@@ -19,6 +19,32 @@ class LoopinData(EGTSRecord):
             *args, **kwargs
         )
 
+    @property
+    def special_inputs(self):
+        def generate_setter(number):
+            flags = self['life_flags']
+            predecessors = 0
+            for i in xrange(1, number, 2):
+                if flags['life{}'.format(i)].value or flags['life{}'.format(i+1)].value:
+                    predecessors += 1
+
+            def setter(value):
+                if number % 2:
+                    position = 'lis_odd'
+                else:
+                    position = 'lis_even'
+                if flags['life{}'.format(number)] == 0b1:
+                    self['lis'][predecessors][position] = value
+                else:
+                    new_field = Lis()
+                    new_field[position] = value
+                    self['lis'].insert(predecessors, new_field)
+                    flags['life{}'.format(number)] = 0b1
+
+            return setter
+
+        return {'lis{}'.format(num): generate_setter(num) for num in range(1, 9)}
+
 
 class Lis(BitField):
     """EGTS_SR_LOOPIN_DATA Loop In State (4 bits each)"""
@@ -30,7 +56,7 @@ class Lis(BitField):
         """
         super(Lis, self).__init__(
             # Flags calculated based on fields existance
-            ('lis_odd', Bits(maxlen=4)),
-            ('lis_even', Bits(maxlen=4)),
+            ('lis_odd', Bits(maxlen=4, value=0)),
+            ('lis_even', Bits(maxlen=4, value=0)),
             *args, **kwargs
         )
--- egts/egts_message/service/teledata/passengers_counters.py
+++ egts/egts_message/service/teledata/passengers_counters.py
@@ -12,14 +12,51 @@ class PassengersCounters(EGTSRecord):
         :param kwargs: parent class kwargs
         """
         super(PassengersCounters, self).__init__(
-            # All set outside
             ('flags', Flags()),
+            # All set outside
             ('dpr', presence_flags.PresenceFlags(field_name='dpr')),
-            ('drl', presence_flags.PresenceFlags(field_name='drl')),
+            ('drl', Byte()),
             ('maddr', UShort()),
-            ('pcd', ArrayOfType(of_type=Pcd, maxlen=8, fixed=1)),
+            ('pcd', ArrayOfType(of_type=Pcd, maxlen=8)),
             *args, **kwargs
         )
+
+    @property
+    def special_inputs(self):
+        flags = self['dpr']
+
+        def get_pcd(number):
+            predecessors = 0
+            for i in xrange(1, number):
+                predecessors += flags['dpr{}'.format(i)].value
+
+            if not flags['dpr{}'.format(number)].value:
+                flags['dpr{}'.format(number)] = 0b1
+                self['pcd'].insert(predecessors, [None, None])
+            return self['pcd'][predecessors]
+
+        def pcd_setter(value):
+            for field in value.keys():
+                self[field] = value[field]
+
+        def generate_ipq_setter(number):
+            def setter(value):
+                pcd = get_pcd(number)
+                pcd.value = (value, None)
+
+            return setter
+
+        def generate_opq_setter(number):
+            def setter(value):
+                pcd = get_pcd(number)
+                pcd.value = (None, value)
+
+            return setter
+
+        setters = {'pcd': pcd_setter}
+        setters.update({'ipq{}'.format(num): generate_ipq_setter(num) for num in range(1, 9)})
+        setters.update({'opq{}'.format(num): generate_opq_setter(num) for num in range(1, 9)})
+        return setters
 
 
 class Flags(BitField):
@@ -32,12 +69,13 @@ class Flags(BitField):
         """
         super(Flags, self).__init__(
             ('empty', Bits(maxlen=7, value=0)),
-            ('rdf', Bits(maxlen=1)),
+            # Raw data flag zero by default
+            ('rdf', Bits(maxlen=1, value=0)),
             *args, **kwargs
         )
 
 
 class Pcd(ArrayOfType):
     """EGTS_SR_PASSENGERS_COUNTERS Passenger Counters Data"""
-    def __init__(self):
-        super(Pcd, self).__init__(of_type=Byte, maxlen=2, minlen=2)
+    def __init__(self, *args, **kwargs):
+        super(Pcd, self).__init__(of_type=Byte, maxlen=2, minlen=2, *args, **kwargs)
